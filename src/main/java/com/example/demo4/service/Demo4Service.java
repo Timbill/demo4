066e314178d8c4f4537ff192753d2a778a622c85
Timbill <tbil99@netscape.net>
2019-06-20 14:13:22 -0400

    Changed system.outs

--- src/main/java/com/example/demo4/service/Demo4Service.java
+++ src/main/java/com/example/demo4/service/Demo4Service.java
@@ -1,5 +1,6 @@
 package com.example.demo4.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import com.example.demo4.exception.CarNotFoundException;
 import com.example.demo4.model.Car;
 import com.example.demo4.persistance.CarRepository;
@@ -15,14 +16,18 @@ public class Demo4Service {
     @Autowired
     CarRepository carRepository;
 
+    @Cacheable("getCar")
     public Car getCarInfo(String model) {
+
         Car car = carRepository.findByModel(model);
-        System.out.println("Car model is " + car.getModel());
-        System.out.println("Car colour is " + car.getColor());
+        System.out.println("getCarInfo-Car model is " + car.getModel());
+        System.out.println("getCarInfo-Car colour is " + car.getColor());
         return car;
     }
 
+    @Cacheable("getCars")
     public Iterable<Car>  getCarsInfo() {
+
         Iterable<Car> cars = carRepository.findAll();
      //   List<Car> carsList = iterableToCollection(cars);
         //System.out.println("Car model is " + car.getModel());
@@ -30,12 +35,13 @@ public class Demo4Service {
         return cars;
     }
     public Car setCarInfo(Car car) {
-        System.out.println("Car speed  is " + car.getSpeed());
+
+        System.out.println("setCarInfo-Car speed  is " + car.getSpeed());
         if(car.getSpeed() == (150))throw new CarNotFoundException();
 
-        System.out.println("Car model is " + car.getModel());
-        System.out.println("Car colour is " + car.getColor());
-        System.out.println("Car speed  is " + car.getSpeed());
+        System.out.println("setCarInfo-Car model is " + car.getModel());
+        System.out.println("setCarInfo-Car colour is " + car.getColor());
+        System.out.println("setCarInfo-Car speed  is " + car.getSpeed());
 
         carRepository.save(car);
         return car;
