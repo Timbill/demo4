27861992348249d1c6f5cb40c1b6b0cedd155a15
Timbill <tbil99@netscape.net>
2019-06-20 14:13:43 -0400

    Changed system.outs

--- src/main/java/com/example/demo4/Demo4Application.java
+++ src/main/java/com/example/demo4/Demo4Application.java
@@ -2,8 +2,10 @@ package com.example.demo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Demo4Application {
 
 	public static void main(String[] args) {
