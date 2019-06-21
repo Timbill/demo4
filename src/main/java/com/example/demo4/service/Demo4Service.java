f694c306038ada8ab5cb9ce4cc219a6b9c0380b8
Timbill <tbil99@netscape.net>
2019-06-21 14:13:16 -0400

    Lombok changes

--- src/main/java/com/example/demo4/service/Demo4Service.java
+++ src/main/java/com/example/demo4/service/Demo4Service.java
@@ -7,16 +7,13 @@ import com.example.demo4.persistance.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Collection;
-
 @Service
 public class Demo4Service {
 
     @Autowired
     CarRepository carRepository;
 
-    @Cacheable("getCar")
+//    @Cacheable("getCar")
     public Car getCarInfo(String model) {
 
         Car car = carRepository.findByModel(model);
@@ -25,13 +22,11 @@ public class Demo4Service {
         return car;
     }
 
-    @Cacheable("getCars")
+//    @Cacheable("getCars")
     public Iterable<Car>  getCarsInfo() {
 
         Iterable<Car> cars = carRepository.findAll();
-     //   List<Car> carsList = iterableToCollection(cars);
-        //System.out.println("Car model is " + car.getModel());
-        //System.out.println("Car colour is " + car.getColor());
+
         return cars;
     }
     public Car setCarInfo(Car car) {
@@ -44,6 +39,23 @@ public class Demo4Service {
         System.out.println("setCarInfo-Car speed  is " + car.getSpeed());
 
         carRepository.save(car);
+        return car;
+    }
+    public Car updateCarInfo(Car car) {
+
+        System.out.println("changeCarInfo-Car speed  is " + car.getSpeed());
+        if(car.getSpeed() == (150))throw new CarNotFoundException();
+
+        Car carPersisted = carRepository.findByModel(car.getModel());
+        System.out.println("changeCarInfo-Car model is " + car.getModel());
+        System.out.println("changeCarInfo-Car colour is " + car.getColor());
+        System.out.println("changeCarInfo-Car speed  is " + car.getSpeed());
+        carPersisted.setModel(car.getModel());
+        carPersisted.setSpeed(car.getSpeed());
+        carPersisted.setModel(car.getModel());
+        carPersisted.setType(car.getType());
+
+        carRepository.save(carPersisted);
         return car;
     }
 }
