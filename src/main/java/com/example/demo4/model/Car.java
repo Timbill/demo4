9fa202e7bbceaff28e95601444436210e45ff575
Timbill <tbil99@netscape.net>
2019-06-21 14:12:02 -0400

    Changes

--- src/main/java/com/example/demo4/model/Car.java
+++ src/main/java/com/example/demo4/model/Car.java
@@ -1,11 +1,17 @@
 package com.example.demo4.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class Car {
 
     @Id
@@ -17,62 +23,4 @@ public class Car {
     private String color;
     private int speed;
 
-    public long getId () {
-        return this.id;
-    }
-
-    public void setId (final long id) {
-        this.id = id;
-    }
-
-    public String getType () {
-        return this.type;
-    }
-
-    public void setType (final String type) {
-        this.type = type;
-    }
-
-    public String getModel () {
-        return this.model;
-    }
-
-    public void setModel (final String model) {
-        this.model = model;
-    }
-
-    public String getColor () {
-        return this.color;
-    }
-
-    public void setColor (final String color) {
-        this.color = color;
-    }
-
-    public int getSpeed () {
-        return this.speed;
-    }
-
-    public void setSpeed (final int speed) {
-        this.speed = speed;
-    }
-
-    // constructor
-    public Car() {
-
-    }
-
-    public Car(String type, String model, String color) {
-        this.type = type;
-        this.model = model;
-        this.color = color;
-    }
-
-    // methods
-    int increaseSpeed(int increment) {
-        this.speed = this.speed + increment;
-        return this.speed;
-    }
-
-    // ...
 }
